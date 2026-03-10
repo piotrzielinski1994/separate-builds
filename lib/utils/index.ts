d936ed8a75f7a9250b17d97e18757cb521946933
Piotr Zieliński <piotr.zielinski.1994@gmail.com>
2026-03-10 23:15:56 +0100

    Replace turborepo with nx to support autodetecting dependencies

--- lib/utils/index.ts
+++ lib/utils/index.ts
@@ -1,3 +1,3 @@
-const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms + 1));
 
 export { wait };
